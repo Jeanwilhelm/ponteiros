c2a42b240827b000dd1292dfba2541c380e2a525
Jeanwilhelm <jean-wilhelm@outlook.com>
2016-09-09 22:23:29 -0300

    Typedef struct - ponteiros para strings

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 //*************************************************************************************************
 //**                                  Autor: Jean Emílio Wilhelm                                 **
 //*************************************************************************************************
-//**    Função: Programa utilizado para iniciar os estudos com conceito básico em ponteiros      **
+//**    Função: Programa utilizado para iniciar os estudos com conceito básico em ponteiro       **
+//**                            Ponteiros com strings e typedef structs                          **
 //*************************************************************************************************
 //**                          Arquivo alterado em 09 de setembro de 2016                         **
 //**                     Aula de Laboratório de Algorítmos - Segundo Semestre                    **
@@ -10,23 +11,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//typedef = define um tipo de
+typedef struct 
+{
+	int indice;
+	char nome[50];
+	char telefone[15];
+	char email[100];
+} entrada;
+
 int main()
 {
-	//criacao e inicializacao das variaveis e ponteiros
+	//criação e inicialização das variáveis e ponteiros
+	entrada pagina_01;
+	entrada *P = &pagina_01;
+	//pagina_01.indice = 1;
+	//pagina_01.nome = {"Jean Emilio Wilhelm"};
+	//pagina_01.telefone = {"(55) 9650-6141"};
 	
-	//char MinhaString[] = {"1234567890"};
-	char MinhaString[] = {'1','2','3','4','5','6','7','8','9','0'};
-	char *MeuPonteiro = &MinhaString;
+	// os dois tipos de scanf funcionam !!!
 	
-	// exibe o valor contido em cada posicao da string a partir do endereço contido no ponteiro
-	do
-	{
-		printf("\nO MeuPonteiro aponta para o valor: %c\n", *MeuPonteiro);
-		MeuPonteiro++;
-	}
-	while(*MeuPonteiro != 0);
+	printf("\nInsira a entrada: ");
+	//scanf("%i", &P->indice);
+	scanf("%i", &(*P).indice);
+
+	printf("\nInsira o Nome: ");	
+	//scanf("%s", &P->nome);
+	scanf("%s", &(*P).nome);
+		
+	printf("\nInsira o Telefone: ");
+	//scanf("%s", &P->telefone);
+	scanf("%s", &(*P).telefone);
 	
-	printf("\nO tamanho de MinhaString e: %i\n", sizeof(MinhaString));
+	printf("\nInsira o email: ");	
+	//scanf("%s", &P->email);
+	scanf("%s", &(*P).email);
+	
+	
+	printf("\nEntrada: %i", pagina_01.indice);
+	printf("\nNome: %s", pagina_01.nome);
+	printf("\nTelefone: %s", pagina_01.telefone);
+	printf("\nEmail: %s", pagina_01.email);
 	
 	// retorna 0
 	return 0;
